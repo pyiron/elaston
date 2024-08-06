df7bb56269c9f46c3504c5226a24aac26b2c6571
samwaseda <o.waseda@mpie.de>
2024-08-06 07:26:55 +0000

    stabilize orthonormalization

--- elaston/tools.py
+++ elaston/tools.py
@@ -21,7 +21,8 @@ def normalize(x):
 
 
 def orthonormalize(vectors):
-    x = normalize(np.asarray(vectors))
+    x = np.eye(3)
+    x[:2] = normalize(np.asarray(vectors)[:2])
     x[1] = x[1] - np.einsum("i,i,j->j", x[0], x[1], x[0])
     x[2] = np.cross(x[0], x[1])
     if np.isclose(np.linalg.det(x), 0):
