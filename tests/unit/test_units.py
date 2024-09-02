979a0a0e73f422a80a290bffd072d83156fcdd6b
samwaseda <o.waseda@mpie.de>
2024-09-02 09:00:32 +0000

    add tests for optional units

--- tests/unit/test_units.py
+++ tests/unit/test_units.py
@@ -1,12 +1,10 @@
 import numpy as np
 import unittest
-from elaston.units import units
+from elaston.units import units, optional_units
 from pint import UnitRegistry
 
 
-@units(
-    inputs={"b": "angstrom", "x": "angstrom", "C": "GPa"}, outputs="GPa"
-)
+@units(inputs={"b": "angstrom", "x": "angstrom", "C": "GPa"}, outputs="GPa")
 def get_stress_absolute(b, x, C):
     return np.round(b / x * C, decimals=8)
 
@@ -26,46 +24,59 @@ def no_units(a):
     return a
 
 
+@units(
+    outputs=lambda distance, time, duration: distance.u / optional_units(time, duration)
+)
+def get_velocity(distance, time=None, duration=None):
+    if time is not None:
+        return distance / time
+    if duration is not None:
+        return distance / duration
+
+
 class TestTools(unittest.TestCase):
     def test_units(self):
         self.assertEqual(get_stress_absolute(1, 1, 1), 1)
         self.assertEqual(get_stress_relative(1, 1, 1), 1)
         ureg = UnitRegistry()
         self.assertEqual(
-            get_stress_absolute(
-                1 * ureg.angstrom, 1 * ureg.angstrom, 1 * ureg.GPa
-            ),
-            1 * ureg.GPa
+            get_stress_absolute(1 * ureg.angstrom, 1 * ureg.angstrom, 1 * ureg.GPa),
+            1 * ureg.GPa,
         )
         self.assertEqual(
-            get_stress_absolute(
-                1 * ureg.nanometer, 1 * ureg.angstrom, 1 * ureg.GPa
-            ),
-            10 * ureg.GPa
+            get_stress_absolute(1 * ureg.nanometer, 1 * ureg.angstrom, 1 * ureg.GPa),
+            10 * ureg.GPa,
         )
         self.assertEqual(
-            get_stress_relative(
-                1 * ureg.angstrom, 1 * ureg.angstrom, 1 * ureg.GPa
-            ),
-            1 * ureg.GPa
+            get_stress_relative(1 * ureg.angstrom, 1 * ureg.angstrom, 1 * ureg.GPa),
+            1 * ureg.GPa,
         )
         self.assertEqual(
-            get_stress_relative(
-                1 * ureg.nanometer, 1 * ureg.angstrom, 1 * ureg.GPa
-            ),
-            1 * ureg.nanometer / ureg.angstrom * ureg.GPa
+            get_stress_relative(1 * ureg.nanometer, 1 * ureg.angstrom, 1 * ureg.GPa),
+            1 * ureg.nanometer / ureg.angstrom * ureg.GPa,
         )
         with self.assertWarns(SyntaxWarning):
             get_stress_relative(1 * ureg.nanometer, 1 * ureg.angstrom, 1)
         self.assertEqual(
             get_multiple_outputs(1 * ureg.angstrom, 1 * ureg.angstrom),
-            (2 * ureg.angstrom, 1 * ureg.angstrom ** 2)
+            (2 * ureg.angstrom, 1 * ureg.angstrom**2),
         )
         with self.assertRaises(ValueError):
             # No relative output units and absolute input units at the same time
             units(outputs=lambda x: x.u, inputs={"x": "GPa"})
         self.assertEqual(no_units(1), 1)
         self.assertEqual(no_units(1 * ureg.angstrom), 1)
+
+    def test_optional_units(self):
+        ureg = UnitRegistry()
+        self.assertEqual(
+            get_velocity(distance=1 * ureg.angstrom, time=1 * ureg.second),
+            1 * ureg.angstrom / ureg.second,
+        )
+        self.assertEqual(
+            get_velocity(distance=1 * ureg.angstrom, duration=1 * ureg.second),
+            1 * ureg.angstrom / ureg.second,
+        )
 
 
 if __name__ == "__main__":
