6b21c1547e68f91763b46eb4f5e3f26f08804c33
samwaseda <o.waseda@mpie.de>
2025-01-06 07:39:19 +0000

    check Zener ratio with units

--- tests/unit/test_constants.py
+++ tests/unit/test_constants.py
@@ -1,6 +1,9 @@
 import unittest
 from elaston import elastic_constants as ec
 import numpy as np
+from pint import UnitRegistry
+
+ureg = UnitRegistry()
 
 data = {
     "Fe": {
@@ -82,7 +85,9 @@ class TestConstants(unittest.TestCase):
         self.assertTrue(ec.is_cubic(C))
         C = ec.initialize_elastic_tensor(**data["W"])
         self.assertTrue(ec.is_cubic(C))
-        C = ec.initialize_elastic_tensor(C_11=211.0, C_12=145.0, C_13=140, C_44=82.0)
+        C = ec.initialize_elastic_tensor(
+            C_11=211.0, C_12=145.0, C_13=140, C_44=82.0
+        )
         self.assertFalse(ec.is_cubic(C))
 
     def test_zener_ratio(self):
@@ -92,7 +97,13 @@ class TestConstants(unittest.TestCase):
         self.assertAlmostEqual(ec.get_zener_ratio(C), 1)
         C = ec.initialize_elastic_tensor(C_11=211.0, C_44=82.0)
         self.assertTrue(ec.is_isotropic(C))
-        C = ec.initialize_elastic_tensor(C_11=211.0, C_12=145.0, C_13=140, C_44=82.0)
+        C = ec.initialize_elastic_tensor(
+            C_11=211.0 * ureg.gigapascal, C_44=82.0 * ureg.gigapascal
+        )
+        self.assertTrue(ec.is_isotropic(C))
+        C = ec.initialize_elastic_tensor(
+            C_11=211.0, C_12=145.0, C_13=140, C_44=82.0
+        )
         self.assertRaises(ValueError, ec.get_zener_ratio, C)
 
     def test_voigt_average(self):
